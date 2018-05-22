a5798aa72f225f26ed375aaef51525e786f6b604
Jegadeesh <jegadeeshkannan@gmail.com>
2018-05-22 18:53:29 +0530

    Initial Version

---
+++ AngularMatApp/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [BrowserModule,MatButtonModule, MatToolbarModule],
+  exports: [MatButtonModule, MatToolbarModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
