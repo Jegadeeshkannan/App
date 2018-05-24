b42d360ebc4cd36defc2b3fd096966c3b83735b4
Jegadeesh <jegadeeshkannan@gmail.com>
2018-05-24 20:24:01 +0530

    initial version

--- AngularMatApp/src/app/app.module.ts
+++ AngularMatApp/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+import { routing } from './app.routing';
+import { BodyComponent } from './body/body.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    HeaderComponent,
+    LeftnavComponent,
+    BodyComponent,
   ],
-  imports: [BrowserModule,MatButtonModule, MatToolbarModule],
+  imports: [BrowserModule,MatButtonModule, MatToolbarModule,routing],
   exports: [MatButtonModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularMatApp/src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+  
+const appRoutes: Routes = [
+    { path: 'about', component: AboutComponent},
+   
+];
+export const routing = RouterModule.forRoot(appRoutes);
