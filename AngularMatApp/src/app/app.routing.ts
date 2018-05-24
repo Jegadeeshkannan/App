56738bcc011bb823487a0699b8c2e56fae83117d
Jegadeeshkannan <jegadeeshkannan@gmail.com>
2018-05-24 23:41:04 +0530

    Update

--- AngularMatApp/src/app/app.routing.ts
+++ AngularMatApp/src/app/app.routing.ts
@@ -1,8 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-  
+import { LoginComponent } from './login/login.component';
 const appRoutes: Routes = [
-    { path: 'about', component: AboutComponent},
-   
+{ path: 'about', component: AboutComponent},
+{ path: 'login', component: LoginComponent},
+
 ];
 export const routing = RouterModule.forRoot(appRoutes);
